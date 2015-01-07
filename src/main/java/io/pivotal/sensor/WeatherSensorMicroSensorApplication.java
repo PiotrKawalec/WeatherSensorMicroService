d44ddb11d065296db3fcb3fe8c155084f0ea7aa4
EugeneBell <eugene_bell@yahoo.com>
2015-01-07 13:55:11 +0000

    Initial project setup

---
+++ src/main/java/io/pivotal/sensor/WeatherSensorMicroSensorApplication.java
@@ -0,0 +1,16 @@
+package io.pivotal.sensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class WeatherSensorMicroSensorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherSensorMicroSensorApplication.class, args);
+    }
+}
