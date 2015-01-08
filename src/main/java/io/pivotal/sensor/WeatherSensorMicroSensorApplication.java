9d8a223cc74db75d9e18f2cfbda81d4b02d578f3
EugeneBell <eugene_bell@yahoo.com>
2015-01-08 16:48:46 +0000

    Added weather (temperature + humidity) sensor feature by integrating rabbitmq and mongodb

--- src/main/java/io/pivotal/sensor/WeatherSensorMicroSensorApplication.java
+++ src/main/java/io/pivotal/sensor/WeatherSensorMicroSensorApplication.java
@@ -1,7 +1,17 @@
 package io.pivotal.sensor;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +20,45 @@ import org.springframework.context.annotation.Configuration;
 @EnableAutoConfiguration
 public class WeatherSensorMicroSensorApplication {
 
+	final static String queueName = "arduino-weather-sensor-queue";
+	
+	@Autowired
+	RabbitTemplate rabbitTemplate;
+	
+	@Bean
+	Queue queue() {
+		return new Queue(queueName, false);
+	}
+
+	@Bean
+	TopicExchange exchange() {
+		return new TopicExchange("arduino-weather-exchange");
+	}
+
+	@Bean
+	Binding binding(Queue queue, TopicExchange exchange) {
+		return BindingBuilder.bind(queue).to(exchange).with(queueName);
+	}
+	
+	@Bean
+	SimpleMessageListenerContainer container(ConnectionFactory connectionFactory, MessageListenerAdapter listenerAdapter) {
+		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+		container.setConnectionFactory(connectionFactory);
+		container.setQueueNames(queueName);
+		container.setMessageListener(listenerAdapter);
+		return container;
+	}
+	
+	@Bean
+	TempHumidityReceiver receiver() {
+        return new TempHumidityReceiver();
+    }
+
+	@Bean
+	MessageListenerAdapter listenerAdapter(TempHumidityReceiver receiver) {
+		return new MessageListenerAdapter(receiver, "receiveMessage");
+	}
+	
     public static void main(String[] args) {
         SpringApplication.run(WeatherSensorMicroSensorApplication.class, args);
     }
