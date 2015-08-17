1b25fb8966ee75f105209495a44676d363b32e20
P K <pkawalec@pivotal.io>
2015-08-17 17:12:07 +0100

    configuration refactorized

--- src/main/java/io/pivotal/sensor/WeatherSensorMicroSensorApplication.java
+++ src/main/java/io/pivotal/sensor/WeatherSensorMicroSensorApplication.java
@@ -20,11 +20,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan
 @SpringBootApplication
 @EnableEurekaClient
-@EnableDiscoveryClient
 public class WeatherSensorMicroSensorApplication {
 
 	final static String queueName = "arduino-weather-queue";
